0473968aae75dc16f1ebfa76224cd0e12a31fdaa
i4mhmh <i4mhmh@outlook.com>
2024-11-19 23:54:31 +0800

    front-change-11.19

--- front-end/src/views/Dashboard.vue
+++ front-end/src/views/Dashboard.vue
@@ -2,55 +2,81 @@
   <title>HLearn-侧信道攻击控制台</title>
   <div class="common-layout">
     <el-container>
-      <el-header
-        ><el-menu
+      <el-header style="padding: 0%">
+        <el-menu
           :default-active="activeIndex"
           class="el-menu-demo"
           mode="horizontal"
           :ellipsis="false"
           @select="handleSelect"
         >
-          <el-menu-item index="0" @select="handleLogoSelect">
+          <el-menu-item index="0">
             <img
-              style="width: 50px; height: 30px"
-              src="@/images/logo.jpg"
+              style="width: 80px"
+              src="@/images/logo.svg"
               alt="HLearn logo"
             />
           </el-menu-item>
-          <el-menu-item index="1">Processing Center</el-menu-item>
-          <el-sub-menu index="2">
-            <template #title>Workspace</template>
-            <el-menu-item index="2-1">item one</el-menu-item>
-            <el-menu-item index="2-2">item two</el-menu-item>
-            <el-menu-item index="2-3">item three</el-menu-item>
-            <el-sub-menu index="2-4">
-              <template #title>item four</template>
-              <el-menu-item index="2-4-1">item one</el-menu-item>
-              <el-menu-item index="2-4-2">item two</el-menu-item>
-              <el-menu-item index="2-4-3">item three</el-menu-item>
-            </el-sub-menu>
+          <el-sub-menu index="1">
+            <template #title> 个人中心 </template>
+            <el-menu-item index="1-0">个人信息</el-menu-item>
+            <el-menu-item index="1-1">执行任务</el-menu-item>
           </el-sub-menu>
+          <el-menu-item index="2">退出登录</el-menu-item>
         </el-menu>
       </el-header>
-      <el-container>
-        <el-aside width="200px">
-          <el-scrollba>
-            <el-menu :default-openeds="['1', '3']">
-              <el-sub-menu index="1">
-                <template #title>
-                  <el-icon>
-                    <message />
-                  </el-icon>
-                </template>
-              </el-sub-menu>
-            </el-menu>
-          </el-scrollba>
+      <el-container style="display: flex">
+        <el-aside style="width: 200px">
+          <el-row class="tac">
+            <el-col>
+              <el-menu
+                default-active="2"
+                class="el-menu-vertical-demo"
+                @open="handleOpen"
+                @close="handleClose"
+                style="height: 83vh"
+              >
+                <el-sub-menu index="1">
+                  <template #title>
+                    <el-icon><location /></el-icon>
+                    <span>主控台</span>
+                  </template>
+                  <el-menu-item-group title="Group One">
+                    <el-menu-item index="1-1">item one</el-menu-item>
+                    <el-menu-item index="1-2">item two</el-menu-item>
+                  </el-menu-item-group>
+                  <el-menu-item-group title="Group Two">
+                    <el-menu-item index="1-3">item three</el-menu-item>
+                  </el-menu-item-group>
+                  <el-sub-menu index="1-4">
+                    <template #title>item four</template>
+                    <el-menu-item index="1-4-1">item one</el-menu-item>
+                  </el-sub-menu>
+                </el-sub-menu>
+                <el-menu-item index="2">
+                  <el-icon><icon-menu /></el-icon>
+                  <span>Navigator Two</span>
+                </el-menu-item>
+                <el-menu-item index="3" disabled>
+                  <el-icon><document /></el-icon>
+                  <span>Navigator Three</span>
+                </el-menu-item>
+                <el-menu-item index="4">
+                  <el-icon><setting /></el-icon>
+                  <span>Navigator Four</span>
+                </el-menu-item>
+              </el-menu>
+            </el-col>
+          </el-row>
         </el-aside>
-        <el-container>
-          <el-main>Main</el-main>
-          <el-footer>HLearn By M0nk3y</el-footer>
-        </el-container>
+        <el-main>Main</el-main>
       </el-container>
+      <el-footer style="background-color: aliceblue; align-items: center">
+        <center>
+          侧信道攻击控制平台 &copy; 2024 <br />Made by Minghui Hou & Guilin
+          University of Electronic Technology
+        </center>
+      </el-footer>
     </el-container>
   </div>
 </template>
@@ -130,5 +156,8 @@ body,
 }
 .el-main {
   flex: 1;
+}
+.el-menu--horizontal > .el-menu-item:nth-child(1) {
+  margin-right: auto;
 }
 </style>
